5d1b4c92827d7c7d00ae65572f843bea79f69f75
Leo Chen <lchen@equuscs.com>
2020-12-07 13:59:55 +0800

    Add a button

--- XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
+++ XamarinApp/XamarinApp/XamarinApp/MainPage.xaml.cs
@@ -14,5 +14,13 @@ namespace XamarinApp
         {
             InitializeComponent();
         }
+
+        private int count = 0;
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+            ((Button) sender).Text = $"You clicked {count} times";
+        }
     }
 }
